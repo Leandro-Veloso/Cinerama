74f6698546807b5f18e50763a1f238393cc301f5
leandroveloso <leandrovdsantos96@gmail.com>
2022-01-24 01:37:56 -0300

    Barra de navegação

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,22 @@
+import {ReactComponent as GithubIcon} from 'assets/img/github.svg';
+import './styles.css'
+function Navbar(){
+
+    return(
+        <header>
+        <nav className="container">
+            <div className="cinerama-nav-content">
+                <h1>Cinerama</h1>
+                <a href="https://github.com/wgustavosantos" target="_blank" rel="noreferrer">
+                    <div className="cinerama-contact-container">
+                        <GithubIcon />
+                        <p className="cinerama-contact-link">/Cinerama on GitHub</p>
+                    </div>
+                </a>
+            </div>
+        </nav>
+    </header>
+      );
+}
+
+export default Navbar;
