59c10e01318f6d3e549bc0b9e0d39f07242690c8
leandroveloso <leandrovdsantos96@gmail.com>
2022-01-25 03:29:03 -0300

    Exibir avaliações dos filmes

--- frontend/src/components/MovieScore/index.tsx
+++ frontend/src/components/MovieScore/index.tsx
@@ -1,15 +1,17 @@
 import MovieStars from "components/MovieStars";
-import './styles.css';
+import './styles.css'
 
-function MovieScore() {
+type Props = {
+    score: number;
+    count: number;
+}
 
-    const score = 3.5;
-    const count = 13;
+function MovieScore({ score, count } : Props) {
 
     return (
         <div className="cinerama-score-container">
             <p className="cinerama-score-value">{score > 0 ? score.toFixed(1) : '-'}</p>
-            <MovieStars />
+            <MovieStars score={score}/>
             <p className="cinerama-score-count">{count} avaliações</p>
         </div>
     );
