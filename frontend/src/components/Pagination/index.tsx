250712303a49724683451e33d06d1bdc7eda66c8
leandroveloso <leandrovdsantos96@gmail.com>
2022-01-25 04:12:51 -0300

    Paginação

--- frontend/src/components/Pagination/index.tsx
+++ frontend/src/components/Pagination/index.tsx
@@ -1,16 +1,24 @@
-import {ReactComponent as Arrow } from 'assets/img/arrow.svg';
+import { ReactComponent as Arrow } from 'assets/img/arrow.svg';
+import { MoviePage } from 'types/movie';
 import './styles.css';
 
-function Pagination() {
+type Props = {
+    page: MoviePage;
+    onChange: Function;
+}
+
+function Pagination({ page, onChange }: Props) {
 
     return (
         <div className="cinerama-pagination-container">
             <div className="cinerama-pagination-box">
-                <button className="cinerama-pagination-button" disabled={true} >
+                <button className="cinerama-pagination-button"
+                    disabled={page.first} onClick={() => onChange(page.number - 1)} >
                     <Arrow />
                 </button>
-                <p>{`${1} de ${3}`}</p>
-                <button className="cinerama-pagination-button" disabled={false} >
+                <p>{`${page.number + 1} de ${page.totalPages}`}</p>
+                <button className="cinerama-pagination-button"
+                    disabled={page.last} onClick={() => onChange(page.number + 1)}>
                     <Arrow className="cinerama-flip-horizontal" />
                 </button>
             </div>
