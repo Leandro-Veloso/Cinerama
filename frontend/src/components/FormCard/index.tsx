af8b16aeb16103718035d0e79b4ca609f1f90037
leandroveloso <leandrovdsantos96@gmail.com>
2022-01-25 04:43:27 -0300

    Salvar avaliação

--- frontend/src/components/FormCard/index.tsx
+++ frontend/src/components/FormCard/index.tsx
@@ -1,15 +1,18 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import axios, { AxiosRequestConfig } from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import { Movie } from 'types/movie';
 import { BASE_URL } from 'utils/requests';
+import { validateEmail } from 'utils/validate';
 import './styles.css'
 
 type Props = {
     movieId: string;
 }
 
-function FormCard( { movieId } : Props) {
+function FormCard({ movieId }: Props) {
+
+    const navigate = useNavigate();
 
     const [movie, setMovie] = useState<Movie>();
 
@@ -20,12 +23,40 @@ function FormCard( { movieId } : Props) {
             });
     }, [movieId]);
 
+    const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+
+        event.preventDefault();
+
+        const email = (event.target as any).email.value;
+
+        const score = (event.target as any).score.value;
+
+        if (!validateEmail(email)) {
+            return;
+        }
+
+        const config: AxiosRequestConfig = {
+            baseURL: BASE_URL,
+            method: 'PUT',
+            url: '/scores',
+            data: {
+                email: email,
+                movieId: movieId,
+                score: score
+            }
+        }
+
+        axios(config).then(response => {
+            navigate("/");
+        });
+    }
+
     return (
         <div className="cinerama-form-container">
             <img className="cinerama-movie-card-image" src={movie?.image} alt={movie?.title} />
             <div className="cinerama-card-bottom-container">
                 <h3>{movie?.title}</h3>
-                <form className="cinerama-form">
+                <form className="cinerama-form" onSubmit={handleSubmit}>
                     <div className="form-group cinerama-form-group">
                         <label htmlFor="email">Informe seu email</label>
                         <input type="email" className="form-control" id="email" />
